22338869cef18b911b1e98cbb72dca280cffd3d2
I2Qi <yararenzo@gmail.com>
2016-11-24 15:12:58 +0900

    とりあえずサンプルを作成。 これをwarにしてデプロイすれば一応動く

---
+++ TestGradle/src/main/java/jp/i2qi/jersey/ap/JerseySampleApplication.java
@@ -0,0 +1,9 @@
+package jp.i2qi.jersey.ap;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("sampleresources")
+public class JerseySampleApplication extends Application {
+
+}
